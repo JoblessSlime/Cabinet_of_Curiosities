95c0fb63c71213dc4c62047516ba959b1955e001
JoblessSlime <raphael.thierry@e-artsup.net>
2025-03-27 18:47:17 +0100

    système de troc (sans payer)
    Le système de troc, sans niveau de rareté ni de prix actuellement (j'implémente ce soir)

--- Cabinet of curiosities/Assets/Scripts/Troc/ChangeCustomer.cs
+++ Cabinet of curiosities/Assets/Scripts/Troc/ChangeCustomer.cs
@@ -1,24 +1,77 @@
 using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 
 public class ChangeCustomer : MonoBehaviour
 {
     public List<GameObject> list_Customers;
+    private List<GameObject> list_CustomersInGame;
 
     public GameObject gameObject_activeCustomer;
-    public GameObject gameObject_nextCustomer;
 
-    public GameObject gameObject_BoxText;
-    public GameObject gameObject_BoxName;
+    public GameObject gameObject_ObjectToBuyParent;
     public GameObject gameObject_ObjectToBuy;
+    public GameObject gameObject_CustomerObject;
+
+    public TextMeshProUGUI TMP_BoxText;
+    public TextMeshProUGUI TMP_BoxName;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject_activeCustomer = list_Customers[random];
+        list_CustomersInGame = list_Customers;
+
+        for (int i = 0; i < list_Customers.Count; i++)
+        {
+            list_Customers[i].SetActive(false);
+        }
+
+        gameObject_activeCustomer = list_Customers[Random.Range(0, list_Customers.Count)];
+        gameObject_activeCustomer.SetActive(true);
+
+        ChangeInterface(gameObject_activeCustomer);
+    }
+
+    public void ChangeInterface(GameObject new_Customer)
+    {
+        new_Customer.GetComponent<Animator>().Play("Anim_CustomerEnter");
+
+        TMP_BoxText.text = new_Customer.GetComponent<Customer>().text;
+        TMP_BoxName.text = new_Customer.GetComponent<Customer>().name;
+
+        gameObject_CustomerObject = new_Customer.GetComponent<Customer>().list_objectsToBuy[Random.Range(0, new_Customer.GetComponent<Customer>().list_objectsToBuy.Count)];
+        gameObject_ObjectToBuy = Instantiate(gameObject_CustomerObject, gameObject_ObjectToBuyParent.transform);
+        // sprite_ObjectToBuy = new_Customer.GetComponent<Customer>().objectToBuy.GetComponent<Sprite>();
     }
 
     public void Change()
     {
+        gameObject_activeCustomer.GetComponent<Animator>().SetBool("Bool_CustomerLeaving", true);
+
+        list_CustomersInGame.Remove(gameObject_activeCustomer);
+        gameObject_activeCustomer = list_CustomersInGame[Random.Range(0, list_CustomersInGame.Count)];
+        gameObject_activeCustomer.SetActive(true);
+
+        ChangeInterface(gameObject_activeCustomer);
+    }
+
+    public void Buy()
+    {
+        CollectionManager manager = FindObjectOfType<CollectionManager>();
+        if (manager != null)
+        {
+            // payer
+
+            Debug.Log($"Collecting {gameObject_ObjectToBuy.tag}");
+            manager.CollectItem(gameObject_ObjectToBuy.tag);
+        }
+        else
+        {
+            Debug.LogError("CollectionManager not found in the scene!");
+        }
+
+        Change();
 
     }
 }
--- Cabinet of curiosities/Assets/Scripts/Troc/Customer.cs
+++ Cabinet of curiosities/Assets/Scripts/Troc/Customer.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Customer : MonoBehaviour
 {
+    public string text;
+    public string name;
+    public List<GameObject> list_objectsToBuy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
